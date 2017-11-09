7bd0611cb3c0703c1bb0c9810510d9094c6d17e7
Aries Wu <aries@bangli.uk>
2017-11-09 23:06:20 +0000

    Fix table name

--- app/Models/FeAdvertise.php
+++ app/Models/FeAdvertise.php
@@ -8,4 +8,16 @@ namespace App\Models;
 class FeAdvertise extends Advertise
 {
     protected $table = 'fe_view_ads';
+
+    public function simpleColumns()
+    {
+        return ['fe_view_ads.id', 'channel_id', 'status', 'position', 'location',
+            'rank', 'title', 'image_url', 'target_url', 'starts', 'ends'];
+    }
+
+    public function fullColumns()
+    {
+        // All columns are needed by default
+        return null;
+    }
 }
